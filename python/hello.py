1b5dc4b39a7077e4383f10e54c839bfe7d0e7fcf
Ronit Kapoor <ronitkapoor@Ronits-MacBook-Air-3.local>
2024-10-30 15:40:02 -0700

    first

--- python/hello.py
+++ python/hello.py
@@ -9,9 +9,12 @@ def hi():
 def three():
     return "three"
 
-def three():
+def two():
     return "two"
 
+
+def four():
+    return "four"
 
 
 if __name__ == "__main__":
@@ -19,3 +22,4 @@ if __name__ == "__main__":
         print(greet())
     for i in range(2):
         print(hi())
+    print(four())
