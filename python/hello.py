ba82d9c0698e89442f4ff1b34a9f77d0bb573ca3
Ronit Kapoor <ronitkapoor@Ronits-MacBook-Air-3.local>
2024-10-30 14:48:07 -0700

    first

--- python/hello.py
+++ python/hello.py
@@ -2,9 +2,12 @@ def greet():
     if 2%1 == 0:
         print("what")
     return "Hello, Python!"
+def hi():
+    return "hey"
 
 
 
 if __name__ == "__main__":
     for i in range(5):
         print(greet())
+    print(hi())
