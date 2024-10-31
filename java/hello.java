3ef241de5b6387a7f5c93c67cfc2d02dcd93846a
Ronit Kapoor <ronitkapoor@Ronits-MacBook-Air-3.local>
2024-10-31 14:26:16 -0700

    first

--- java/hello.java
+++ java/hello.java
@@ -3,6 +3,7 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("Hello, Java!");
         System.out.println("Hi");
+        System.out.println("yoooooo!");
     }
 
     public static String greet() {
